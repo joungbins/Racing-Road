8468ce5973c5b522c9ada50e88394eef03dd4c4a
joungbins <86279518+joungbins@users.noreply.github.com>
2021-07-28 15:56:51 +0900

    1.0

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async Game_event_Event5_Act1(runtime, localVars)
+		{
+			//Enter your rewarded ad codes here
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
